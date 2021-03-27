0c8c5974d2ef15073db0812447993702dc980311
Yamita123-CELL <yamitabasant@gmail.com>
2021-03-27 22:02:56 +0530

    commited

---
+++ src/Sdata.js
@@ -0,0 +1,46 @@
+const Sdata = [
+    {
+        imageSrc : "https://cdn1.matadornetwork.com/blogs/1/2018/02/Taj-Mahal.jpg", 
+        location:"Agra,Uttar Pradesh,India",
+        sname : "Taj Mahal",
+        link : "https://en.wikipedia.org/wiki/Taj_Mahal",
+    },
+    {
+        imageSrc : "https://cdn.britannica.com/s:800x1000/54/150754-050-5B93A950/statue-Christ-the-Redeemer-Rio-de-Janeiro.jpg", 
+        location:"Corcovado mountain,Rio de Janeiro,Brazil",
+        sname : "Christ the Redeemer",
+        link : "https://en.wikipedia.org/wiki/Christ_the_Redeemer_(statue)",
+    },
+    {
+        imageSrc : "https://discovery.sndimg.com/content/dam/images/discovery/editorial/Curiosity/2020/2/18/New_Seven_Wonders_World_Shutterstock2.png.rend.hgtvcom.476.268.suffix/1582495198497.png", 
+        location:"Regio III Isis et Serapis,Rome,Italy",
+        sname : "Colosseum",
+        link : "https://en.wikipedia.org/wiki/Colosseum",
+    },
+    {
+        imageSrc : "https://cdn.britannica.com/s:800x1000/82/94382-050-20CF23DB/Great-Wall-of-China-Beijing.jpg", 
+        location:"China",
+        sname : "Great Wall of China",
+        link : "https://en.wikipedia.org/wiki/Great_Wall_of_China",
+    },
+    {
+        imageSrc : "https://cdn.britannica.com/s:800x1000/49/61349-050-9FFBEB28/El-Castillo-pyramid-plaza-Toltec-state-Yucatan.jpg", 
+        location:"Yucatán,Mexico",
+        sname : "Chichén Itzá",
+        link : "https://en.wikipedia.org/wiki/Chichen_Itza",
+    },
+    {
+        imageSrc : "https://cdn.britannica.com/s:800x1000/25/153525-050-FC43840D/Khaznah-Petra-Jordan.jpg", 
+        location:"Ma'an Governorate,Jordan",
+        sname : "Petra",
+        link : "https://en.wikipedia.org/wiki/Petra",
+    },
+    {
+        imageSrc : "https://cdn.britannica.com/s:800x1000/30/94530-050-99493FEA/Machu-Picchu.jpg", 
+        location:"Peru,Cuzco Region",
+        sname : "Machu Picchu",
+        link : "https://en.wikipedia.org/wiki/Machu_Picchu",
+    },
+]
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,59 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Sdata from './Sdata';
+console.log(Sdata[0].sname);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <section>
+    <span className="gradient"></span>
+    <h1 className="heading_style">Seven Wonders of the World</h1>
+    {Sdata.map((val) => {
+      return(
+        <App 
+            imageSrc = {val.imageSrc} 
+            sname = {val.sname}
+            location={val.location}
+            link = {val.link}
+         />
+      );
+    })}
+    {/* <App imageSrc = {Sdata[0].imageSrc} 
+    title={Sdata[0].title}
+    sname = {Sdata[0].sname}
+    link = {Sdata[0].link}
+    />
+    <App imageSrc = {Sdata[1].imageSrc} 
+    title={Sdata[1].title}
+    sname = {Sdata[1].sname}
+    link = {Sdata[1].link}
+    />
+    <App imageSrc = {Sdata[2].imageSrc} 
+    title={Sdata[2].title}
+    sname = {Sdata[2].sname}
+    link = {Sdata[2].link}
+    />
+    <App imageSrc = {Sdata[3].imageSrc} 
+    title={Sdata[3].title}
+    sname = {Sdata[3].sname}
+    link = {Sdata[3].link}
+    />
+    <App imageSrc = {Sdata[4].imageSrc} 
+    title={Sdata[4].title}
+    sname = {Sdata[4].sname}
+    link = {Sdata[4].link}
+    />
+    <App imageSrc = {Sdata[5].imageSrc} 
+    title={Sdata[5].title}
+    sname = {Sdata[5].sname}
+    link = {Sdata[5].link}
+    />
+    <App imageSrc = {Sdata[6].imageSrc} 
+    title={Sdata[6].title}
+    sname = {Sdata[6].sname}
+    link = {Sdata[6].link}
+    />  */}
+  </section>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +64,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
